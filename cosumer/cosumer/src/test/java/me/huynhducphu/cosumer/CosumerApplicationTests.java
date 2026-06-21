b7ef177f04adb6f0f24bd1aad037409aaa4b2193
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-06-21 14:56:02 +0700

    first init

---
+++ cosumer/cosumer/src/test/java/me/huynhducphu/cosumer/CosumerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.huynhducphu.cosumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
